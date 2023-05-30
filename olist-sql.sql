e6aa1a29ad4839fe4001bdd94d13d5b7de856ff8
akbarfithrawan <133627219+akbarfithrawan@users.noreply.github.com>
2023-05-30 14:56:51 +0700

    Update olist-sql.sql

--- olist-sql.sql
+++ olist-sql.sql
@@ -1,1 +1,9 @@
-
+````sql
+SELECT
+	COUNT (*) FILTER (WHERE customer_id IS NULL) AS null_id,
+	COUNT (*) FILTER (WHERE customer_unique_id IS NULL) AS null_unique_id,
+	COUNT (*) FILTER (WHERE customer_zip_code_prefix IS NULL) AS null_zip_code,
+	COUNT (*) FILTER (WHERE customer_city IS NULL) AS null_city,
+	COUNT (*) FILTER (WHERE customer_state IS NULL) AS null_state
+FROM customers;
+````
